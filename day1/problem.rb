7c395defb810a546ec6e42cd858f7c51bb1283ae
Johnathon Root <john.root@hey.com>
2020-12-01 18:20:49 -0500

    day1

---
+++ day1/problem.rb
@@ -0,0 +1,50 @@
+require 'benchmark'
+
+def getValue(l, h, num)
+	return num[l] + num[h]
+end
+
+def getValue2(l, m, h, num)
+	return num[l] + num[m] + num[h]
+end
+
+numbers = File.readlines('input.txt').map!(&:to_i)
+
+puts Benchmark.measure {
+	numbers.sort!()
+
+	low = 0
+	hi = numbers.count - 1
+	current_value = getValue(low, hi, numbers)
+
+	while (current_value != 2020) do
+		current_value > 2020 ? (hi -= 1) : (low += 1)
+		current_value = getValue(low, hi, numbers)
+	end
+
+	puts "Task 1: #{numbers[low] * numbers[hi]}"
+
+	low = 0
+	hi = numbers.count - 1
+
+	for mid in 1..numbers.count-2 do
+		low = 0
+		hi = numbers.count - 1
+
+		current_value = getValue2(low, mid, hi, numbers)
+		
+		while (current_value != 2020) do
+			current_value > 2020 ? (hi -= 1) : (low += 1)
+			
+			if (hi == mid || low == mid) then 
+				break
+			end
+
+			current_value = getValue2(low, mid, hi, numbers)
+		end
+
+		if (current_value == 2020) then break end
+	end
+
+	puts "Task 2: #{numbers[low] * numbers[mid] * numbers[hi]}"
+}
