b9b4c9a76cc6d8edafd214165d55cf2a772d2b8c
Johnathon Root <john.root@hey.com>
2020-12-02 13:42:52 -0500

    fixing tabs

--- day1/problem.rb
+++ day1/problem.rb
@@ -1,58 +1,58 @@
 require 'benchmark'
 
 def getValue(l, h, num)
-	return num[l] + num[h]
+    return num[l] + num[h]
 end
 
 def getValue2(l, m, h, num)
-	return num[l] + num[m] + num[h]
+    return num[l] + num[m] + num[h]
 end
 
 numbers = File.readlines('input.txt').map!(&:to_i)
 
 # O(nlogn) sort
 puts Benchmark.measure {
-	puts "Sorting!"
-	numbers.sort!()
+    puts "Sorting!"
+    numbers.sort!()
 }
 
 # O(n) search
 puts Benchmark.measure {
-	low = 0
-	hi = numbers.count - 1
-	current_value = getValue(low, hi, numbers)
+    low = 0
+    hi = numbers.count - 1
+    current_value = getValue(low, hi, numbers)
 
-	while (current_value != 2020) do
-		current_value > 2020 ? (hi -= 1) : (low += 1)
-		current_value = getValue(low, hi, numbers)
-	end
+    while (current_value != 2020) do
+        current_value > 2020 ? (hi -= 1) : (low += 1)
+        current_value = getValue(low, hi, numbers)
+    end
 
-	puts "Task 1: #{numbers[low] * numbers[hi]}"
+    puts "Task 1: #{numbers[low] * numbers[hi]}"
 }
 
 # O(n^2) search
 puts Benchmark.measure {
-	low = 0
-	hi = numbers.count - 1
+    low = 0
+   	hi = numbers.count - 1
 
-	for mid in 1..numbers.count-2 do
-		low = 0
-		hi = numbers.count - 1
+    for mid in 1..numbers.count-2 do
+        low = 0
+        hi = numbers.count - 1
 
-		current_value = getValue2(low, mid, hi, numbers)
-		
-		while (current_value != 2020) do
-			current_value > 2020 ? (hi -= 1) : (low += 1)
-			
-			if (hi == mid || low == mid) then 
-				break
-			end
+        current_value = getValue2(low, mid, hi, numbers)
+        
+        while (current_value != 2020) do
+            current_value > 2020 ? (hi -= 1) : (low += 1)
+            
+            if (hi == mid || low == mid) then 
+                break
+            end
 
-			current_value = getValue2(low, mid, hi, numbers)
-		end
+            current_value = getValue2(low, mid, hi, numbers)
+        end
 
-		if (current_value == 2020) then break end
-	end
+        if (current_value == 2020) then break end
+    end
 
-	puts "Task 2: #{numbers[low] * numbers[mid] * numbers[hi]}"
+    puts "Task 2: #{numbers[low] * numbers[mid] * numbers[hi]}"
 }
--- day2/problem.rb
+++ day2/problem.rb
@@ -1,36 +1,36 @@
 def calculateValidPasswords(input, validDefinition)
-	regex = /(\d+)-(\d+) (\w): (\w+)/
+    regex = /(\d+)-(\d+) (\w): (\w+)/
 
-	numValidPasswords = 0
+    numValidPasswords = 0
 
-	input.each do |line|
-		data = regex.match(line)
+    input.each do |line|
+        data = regex.match(line)
 
-		if (method(validDefinition).call(data[1].to_i(), data[2].to_i(), data[3], data[4])) then
-			numValidPasswords += 1
-		end
-	end
+        if (method(validDefinition).call(data[1].to_i(), data[2].to_i(), data[3], data[4])) then
+            numValidPasswords += 1
+        end
+    end
 
-	return numValidPasswords
+    return numValidPasswords
 end
 
 def problemOneDefinition(min, max, expect, password)
-	charMap = Hash.new(0)
+    charMap = Hash.new(0)
 
-	password.chars.each { |char| charMap[char] += 1 }
+    password.chars.each { |char| charMap[char] += 1 }
 
-	return charMap[expect] >= min && charMap[expect] <= max
+    return charMap[expect] >= min && charMap[expect] <= max
 end
 
 def problemTwoDefinition(first, last, expect, password)
-	return (password.chars[first - 1] == expect) ^ (password.chars[last - 1] == expect)
+    return (password.chars[first - 1] == expect) ^ (password.chars[last - 1] == expect)
 end
 
 # test
 test_input = [
-	"1-3 a: abcde",
-	"1-3 b: cdefg",
-	"2-9 c: ccccccccc"
+    "1-3 a: abcde",
+    "1-3 b: cdefg",
+    "2-9 c: ccccccccc"
 ]
 
 input = File.readlines('input')
